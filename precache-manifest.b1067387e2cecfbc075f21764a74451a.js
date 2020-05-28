bbb0e72e21bef550dbcd369f3b1a3c3914dc7f59
pdhung197 <pdhung.info@gmail.com>
2020-05-28 18:14:23 +0700

    Updates

---
+++ precache-manifest.b1067387e2cecfbc075f21764a74451a.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8b3fcddd8e7b33c7cf1b3d945524f41",
+    "url": "/index.html"
+  },
+  {
+    "revision": "37d6d0f9a349da0cdb99",
+    "url": "/static/css/main.c6b8190d.chunk.css"
+  },
+  {
+    "revision": "57656779201892c81960",
+    "url": "/static/js/2.cedefd91.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.cedefd91.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "37d6d0f9a349da0cdb99",
+    "url": "/static/js/main.0dd4d42c.chunk.js"
+  },
+  {
+    "revision": "cade81f3650a1d02ee0b",
+    "url": "/static/js/runtime-main.a1e6d78a.js"
+  },
+  {
+    "revision": "5cc2d89b6d083196db19fca5087ee054",
+    "url": "/static/media/people.5cc2d89b.png"
+  },
+  {
+    "revision": "8d9481f53f59b66ba6d446686b315293",
+    "url": "/static/media/planet.8d9481f5.png"
+  },
+  {
+    "revision": "62a19047e93ca9b4361607e0e27b9921",
+    "url": "/static/media/star-wars-logo-png-3-transparent.62a19047.png"
+  },
+  {
+    "revision": "f7e7871ea92d6fc9ddc8aaf5f3d6d69c",
+    "url": "/static/media/starship.f7e7871e.png"
+  },
+  {
+    "revision": "71f41fb5bde3cdabe22b656523f7ce47",
+    "url": "/static/media/vehicles.71f41fb5.png"
+  }
+]);
