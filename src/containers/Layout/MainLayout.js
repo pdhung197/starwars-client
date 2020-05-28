7601fd878059c4ca79b109f4f4bd4c097b242572
pdhung197 <pdhung.info@gmail.com>
2020-05-28 18:19:32 +0700

    chore(route): Fix wrong route

--- src/containers/Layout/MainLayout.js
+++ src/containers/Layout/MainLayout.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
+import createHistory from "history/createBrowserHistory";
 
 import HomeView from './../Home/HomeView';
 import DataView from './../DataView/DataView';
@@ -8,11 +9,14 @@ import Nav from './../../components/Nav/Nav';
 
 function MainLayout() {
     return (
-        <Router>
+        <Router history={createHistory({ basename: process.env.PUBLIC_URL })}>
             <Nav />
             <section role="main">
                 <Switch>
                     <Route exact path="/" component={HomeView} />
+                    {/* <Route exact path="/starwars-client">
+                        <Redirect to="/" />
+                    </Route> */}
                     <Route path="/data/:dataName" component={DataView} />
                 </Switch>
             </section>
