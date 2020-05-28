9f27b82c63560508faa196394c7b4892cbe63fed
pdhung197 <pdhung.info@gmail.com>
2020-05-28 18:32:16 +0700

    chore(deploy): Deploy app

--- src/containers/Layout/MainLayout.js
+++ src/containers/Layout/MainLayout.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
-import createHistory from "history/createBrowserHistory";
 
 import HomeView from './../Home/HomeView';
 import DataView from './../DataView/DataView';
@@ -9,14 +8,14 @@ import Nav from './../../components/Nav/Nav';
 
 function MainLayout() {
     return (
-        <Router history={createHistory({ basename: process.env.PUBLIC_URL })}>
+        <Router>
             <Nav />
             <section role="main">
                 <Switch>
                     <Route exact path="/" component={HomeView} />
-                    {/* <Route exact path="/starwars-client">
+                    <Route exact path="/starwars-client">
                         <Redirect to="/" />
-                    </Route> */}
+                    </Route>
                     <Route path="/data/:dataName" component={DataView} />
                 </Switch>
             </section>
